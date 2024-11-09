df94a1d18cd3dc63162c524955e2dcceb04497d6
Lavrikov Pavel <laurus.p.a.work@gmail.com>
2024-11-09 13:47:52 +0300

    Add bottom sheet dialog with scrolling compose inside it Now it has bug. When scrolling down bottom sheet is closing. List must be scrolling instead

--- app/src/main/java/com/lauruspa/life_management/features/test/BottomSheetWithScrollingCompose.kt
+++ app/src/main/java/com/lauruspa/life_management/features/test/BottomSheetWithScrollingCompose.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.unit.dp
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.lauruspa.life_management.core.ui.theme.LifeManagementTheme
@@ -28,14 +35,28 @@ internal class BottomSheetWithScrollingCompose : BottomSheetDialogFragment() {
 			setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
 			setContent {
 				LifeManagementTheme {
-					LazyColumn {
-						items(
-							count = 100,
-						) { index ->
-							ListItem(
-								index = index,
-								modifier = Modifier.fillMaxWidth()
-							)
+					val nestedScrollConnection = rememberNestedScrollInteropConnection()
+					Column(
+						modifier = Modifier.nestedScroll(nestedScrollConnection)
+					) {
+						Box(
+							modifier = Modifier
+								.fillMaxWidth()
+								.background(Color.Red)
+								.height(200.dp)
+						)
+						LazyColumn(
+							modifier = Modifier
+								.weight(1f)
+						) {
+							items(
+								count = 100,
+							) { index ->
+								ListItem(
+									index = index,
+									modifier = Modifier.fillMaxWidth()
+								)
+							}
 						}
 					}
 				}
